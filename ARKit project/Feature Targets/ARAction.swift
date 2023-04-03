cce65c61ba2ef98ce36452e58ce6c974f4304950
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-03 15:17:56 -0600

    Add ARAction

---
+++ ARKit project/Feature Targets/ARAction.swift
@@ -0,0 +1,13 @@
+//
+//  ARAction.swift
+//  ARKit project
+//
+//  Created by Marco Ramirez on 3/04/23.
+//
+
+import SwiftUI
+
+enum ARAction {
+    case placeBlock(Color: Color)
+    case removeAllAnchors
+}

4bcbe94b52c0ee642a4a831a5e7626d2eaf8f717
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-03 10:50:38 -0600

    Initial Commit

---
+++ ARKit project/ARKit_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKit_projectApp.swift
+//  ARKit project
+//
+//  Created by Marco Ramirez on 3/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ARKit_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
